4b22608588da08b2e8c2c8b496dbcb4c5028a3bf
adam aubry <a.aubry@draehi.com>
2025-10-11 21:16:40 +0200

    debug attempt clob client

--- src/utils/createClobClient.ts
+++ src/utils/createClobClient.ts
@@ -1,23 +1,26 @@
-import { ethers } from 'ethers';
+import { Wallet } from '@ethersproject/wallet';
 import { ClobClient } from '@polymarket/clob-client';
 import { SignatureType } from '@polymarket/order-utils';
 import { ENV } from '../config/env';
 
 const PROXY_WALLET = ENV.PROXY_WALLET;
 const PRIVATE_KEY = ENV.PRIVATE_KEY;
-const CLOB_HTTP_URL = ENV.CLOB_HTTP_URL;
+//const CLOB_HTTP_URL = ENV.CLOB_HTTP_URL;  --- DEBUG ---
 
 const createClobClient = async (): Promise<ClobClient> => {
     const chainId = 137;
-    const host = CLOB_HTTP_URL as string;
-    const wallet = new ethers.Wallet(PRIVATE_KEY as string);
-    let clobClient = new ClobClient(
+    //const host = CLOB_HTTP_URL as string;  --- DEBUG ---
+    const host = 'https://clob.polymarket.com';
+    const wallet = new Wallet(PRIVATE_KEY as string);
+    const clobClient = new ClobClient(
         host,
         chainId,
-        wallet,
+        wallet
+
+        /*,
         undefined,
         SignatureType.POLY_PROXY,
-        PROXY_WALLET as string
+        PROXY_WALLET as string*/
     );
 
     const originalConsoleError = console.error;
@@ -35,9 +38,10 @@ const createClobClient = async (): Promise<ClobClient> => {
         host,
         chainId,
         wallet,
-        creds,
+        creds
+        /*,
         SignatureType.POLY_PROXY,
-        PROXY_WALLET as string
+        PROXY_WALLET as string*/
     );
     console.log(clobClient);
     return clobClient;
