74c5d997e679a21b2a272c06b2d27165a4060a63
root <root@ip-172-31-19-33.eu-north-1.compute.internal>
2025-10-11 16:45:04 +0000

    final spinner after fuckitweball

--- src/utils/spinner.ts
+++ src/utils/spinner.ts
@@ -1,14 +1,14 @@
 import { createSpinner } from 'nanospinner';
 
 const frames = [
-    '▰▱▱▱▱▱▱',
-    '▰▰▱▱▱▱▱',
-    '▰▰▰▱▱▱▱',
-    '▰▰▰▰▱▱▱',
-    '▰▰▰▰▰▱▱',
-    '▰▰▰▰▰▰▱',
-    '▰▰▰▰▰▰▰',
-    '▱▱▱▱▱▱▱',
+    'o------',
+    'oo-----',
+    'ooo----',
+    'oooo---',
+    'ooowo--',
+    'oooooo-',
+    'ooooooo',
+    '-------',
 ];
 
 let spinnerInstance: ReturnType<typeof createSpinner> | null = null;
