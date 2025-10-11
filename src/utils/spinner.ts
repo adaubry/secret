d2e157d7fd2a49c248e4d1f9a2b57d4eb9f646fd
adam aubry <a.aubry@draehi.com>
2025-10-11 14:54:56 +0200

    remove the dumb ora spinner to compile

--- src/utils/spinner.ts
+++ src/utils/spinner.ts
@@ -1,19 +1,38 @@
-import ora from 'ora';
+const frames: string[] = [
+    '▰▱▱▱▱▱▱',
+    '▰▰▱▱▱▱▱',
+    '▰▰▰▱▱▱▱',
+    '▰▰▰▰▱▱▱',
+    '▰▰▰▰▰▱▱',
+    '▰▰▰▰▰▰▱',
+    '▰▰▰▰▰▰▰',
+    '▱▱▱▱▱▱▱',
+];
 
-const spinner = ora({
-    spinner: {
-        interval: 200,
-        frames: [
-            '▰▱▱▱▱▱▱',
-            '▰▰▱▱▱▱▱',
-            '▰▰▰▱▱▱▱',
-            '▰▰▰▰▱▱▱',
-            '▰▰▰▰▰▱▱',
-            '▰▰▰▰▰▰▱',
-            '▰▰▰▰▰▰▰',
-            '▱▱▱▱▱▱▱',
-        ],
+let i = 0;
+let spinnerInterval: NodeJS.Timeout | null = null;
+
+interface Spinner {
+    start: (text?: string) => void;
+    stop: (text?: string) => void;
+}
+
+const spinner: Spinner = {
+    start: (text = '') => {
+        process.stdout.write(text + ' ');
+        spinnerInterval = setInterval(() => {
+            process.stdout.write('\r' + text + ' ' + frames[i]);
+            i = (i + 1) % frames.length;
+        }, 200);
     },
-});
+
+    stop: (text = '') => {
+        if (spinnerInterval) {
+            clearInterval(spinnerInterval);
+            spinnerInterval = null;
+        }
+        process.stdout.write('\r' + text + '\n');
+    },
+};
 
 export default spinner;
